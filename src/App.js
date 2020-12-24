4804567dd5e1069d704a7e1d5bb341f048bc7c2a
GIRISH-GK <girishgk398@gmail.com>
2020-12-25 03:15:42 +0530

    ToDoList

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./App.css";
+import ListItems from "./ListItems";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      },
+    };
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    
+  }
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.text !== "") {
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: {
+          text: "",
+          key: "",
+        },
+      });
+    }
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  }
+ 
+
+  render() {
+    return (
+      <div className="App">
+       
+        <header>
+          <form className="todo" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Enter list"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            ></input>
+            <button type="submit">Add</button>
+          </form>
+          <p>{this.state.items.text}</p>
+        </header>
+        <ListItems items={this.state.items}/>
+      </div>
+    );
+  }
+}
+
+export default App;
